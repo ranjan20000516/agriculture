2037d8871e64d78bed73cba321192f2693dbcf62
Aryan Barnwal <aryanbarnwal007@gmail.com>
2021-10-16 16:23:29 +0530

    Contact us page added

--- app/src/main/java/com/example/agriculture/Screen_4.java
+++ app/src/main/java/com/example/agriculture/Screen_4.java
@@ -5,8 +5,10 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.agriculture.adapter.InfoAdapter;
@@ -19,6 +21,7 @@ public class Screen_4 extends AppCompatActivity {
     private int id;
     private  String content,name;
     RecyclerView recyclerView;
+    LinearLayout linearLayout;
     DatabaseHandler databaseHandler;
     InfoAdapter adapter;
     TextView textView;
@@ -33,13 +36,21 @@ public class Screen_4 extends AppCompatActivity {
 
         id=getIntent().getIntExtra("id",0);
         content=getIntent().getStringExtra("content");
+        Log.i("contact us",content.toLowerCase());
         name=getIntent().getStringExtra("name");
         getSupportActionBar().setTitle(name);
         appModels= databaseHandler.getModelsbyID(id);
         recyclerView=findViewById(R.id.recycle_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        linearLayout = findViewById(R.id.contactUs);
+
         adapter=new InfoAdapter(this,appModels);
-        if(appModels.size()==0){
+        if(name.toLowerCase().equals("contact us")){
+            recyclerView.setVisibility(View.GONE);
+            textView.setVisibility(View.GONE);
+            linearLayout.setVisibility(View.VISIBLE);
+        }
+       else if(appModels.size()==0){
             textView.setText(content);
             recyclerView.setVisibility(View.GONE);
             textView.setVisibility(View.VISIBLE);
