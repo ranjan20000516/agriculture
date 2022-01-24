ef8e2d17f78596ccaa151ebca03e31517949189b
Krishna Bose <krishna.bose02@gmail.com>
2022-01-24 15:22:00 +0530

    added images and formatted even some more

--- app/src/main/java/com/example/agriculture/SplashActivity.java
+++ app/src/main/java/com/example/agriculture/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.example.agriculture;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,6 +15,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         getSupportActionBar().hide();
 
